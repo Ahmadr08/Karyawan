2b5e28c9e6d96a31ccc4346ca88d9d9044bfca3b
Ahmadr08 <ahmad.2304@students.amikom.ac.id>
2020-04-15 15:34:10 +0700

    Responsi Pemrograman

---
+++ Karyawan.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2913
+{
+    class Karyawan
+    {
+        //properties
+        public int NIK {get; set;}
+        public string Nama {get; set;}
+        public int GajiBln {get; set;} 
+
+        //method
+        public Karyawan(int NIK, string Nama, int Gaji)
+        {
+            this.NIK = NIK;
+            this.Nama = Nama;
+
+            if(Gaji<0)
+                {
+                this.GajiBln = 0;
+                }
+            else
+                {
+                this.GajiBln = Gaji;
+                }
+        }
+            
+        public void Tampil()
+            {
+            Console.WriteLine("{0}\t{1}\t{2}", NIK, Nama, GajiBln);
+            }
+
+        public void GajiNaik()
+            {
+            double tmp = 0;
+            tmp = 0.1 * GajiBln;
+            GajiBln += Convert.ToInt32(tmp);
+            Console.WriteLine("{0}\t{1}\t{2}", NIK, Nama, GajiBln);
+            }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2913
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //objek dari class Karyawan
+            Karyawan saya = new Karyawan(190302123, "Frank" , 3000000);
+            Karyawan saya1 = new Karyawan(190302124, "Nero", 2000000);
+
+            //membuat properties ob
+
+            Console.WriteLine("NIK\t Nama\t\t\t Gaji ");
+            saya.Tampil();
+            saya1.Tampil();
+
+            Console.WriteLine("Asyik Gaji Naik 10%");
+
+            saya.GajiNaik();
+            saya1.GajiNaik();
+
+            Console.ReadKey();
+        }
+    }
+}
